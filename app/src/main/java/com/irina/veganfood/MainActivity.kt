4108f86cff654e60c0f0d7c75673e4da8d09c149
irina <badertdinova289@gmail.com>
2019-12-10 21:07:01 +0700

    Initial commit

---
+++ app/src/main/java/com/irina/veganfood/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.irina.veganfood
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        navigation.setupWithNavController(navController)
+    }
+}
